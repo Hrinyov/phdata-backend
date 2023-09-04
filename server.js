4aa68efc271839ffe66a5e816f5c10b39af65fba
Vlad <hrinyov@gmail.com>
2023-09-04 23:38:55 +0300

    add register and login logic

--- server.js
+++ server.js
@@ -1,18 +1,108 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-import { PrismaClient } from '@prisma/client';
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+
+
 const prisma = new PrismaClient();
+app.use(bodyParser.json());
 app.use(cors());
-app.use('/login', (req, res)=>{
-    res.send({
-        token: 'test1234'
-    });
+
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                username,
+            },
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username already taken' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10); 
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                password: hashedPassword,
+                token: ''
+            },
+        });
+
+        console.log('User registered:', newUser.username);
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).json({ message: 'Registration failed' });
+    }
 });
 
-// create connection to DB;
-// create login logic;
-// generate and save token - create function to generate token;
-// send token to frontend; 
-// send token to DB;
-app.listen(8080,()=> console.log('API is running on http://localhost:8080/login'));
+const checkUserExists = async (req, res, next) => {
+    const { username } = req.body;
+
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                username,
+            },
+        });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        req.user = user; 
+        next();
+    } catch (error) {
+        console.error('Error while checking user existence:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+const checkPassword = async (req, res, next) => {
+    const { password } = req.body;
+    const { user } = req;
+
+    try {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        next();
+    } catch (error) {
+        console.error('Error while checking password:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+app.post('/login', checkUserExists, checkPassword, async (req, res) => {
+    const { user } = req;
+
+    try {
+        const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+            expiresIn: '1h', 
+        });
+
+        await prisma.user.update({
+            where: { id: user.id },
+            data: { token },
+        });
+
+        console.log('User logged in:', user.username);
+        res.status(200).json({ message: 'ok', token });
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.listen(8080,()=> console.log('API is running on http://localhost:8080/'));
