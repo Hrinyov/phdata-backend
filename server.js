0de957523c2aea37cc57984b00a570567a4c0651
Vlad <hrinyov@gmail.com>
2023-09-10 10:17:07 +0300

    add token validation and  s3 bucket

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
+const multer = require('multer');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const crypto = require('crypto');
 
-
+const app = express();
 const prisma = new PrismaClient();
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
+    },
+    region: process.env.BUCKET_LOCATION
+});
+
 app.use(bodyParser.json());
 app.use(cors());
 
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -104,5 +116,56 @@ app.post('/login', checkUserExists, checkPassword, async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
+const validateToken = (req, res, next) => {
+    const token = req.header('x-auth-token'); // Assuming you send the token in a header
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY); // Replace with your secret key
+        req.user = decoded;
+        next();
+    } catch (ex) {
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+}
+
+app.get('/protected', validateToken, (req, res) => {
+    res.json({ message: 'Valid token. Access granted.' });
+
+});
+
+const randomName = () => {
+    return crypto.randomBytes(32).toString('hex');
+}
+
+app.post('/post', validateToken, upload.single('image'), async (req, res)=>{
+    console.log('req.body', req.body)
+    console.log('req.file', req.file)
+
+    const userId = req.user.userId
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: randomName(),
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+    }
+    const command = new PutObjectCommand(params)
+    
+    await s3.send(command)
+    const post = await prisma.image.create({
+        data:{
+            imageName: params.Key,
+            description: req.body.description,
+            authorId: userId
+        }
+    })
+    res.send(post)
+})
+
+
 
 app.listen(8080,()=> console.log('API is running on http://localhost:8080/'));
